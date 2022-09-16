0dbc527093c0f506cb7888453377cd9899213389
canasmh <canasmh@yahoo.com>
2022-09-15 22:08:52 -0400

    Complete request 1 of topic 2

--- index.js
+++ index.js
@@ -70,8 +70,10 @@ const zooAnimals = [
   💡 NOTE: the array returned should be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(zooAnimals){
+    let newArray = [];
+    zooAnimals.forEach((animal) => newArray.push(`name: ${animal.animal_name}, scientific: ${animal.scientific_name}`))
+    return newArray;
   }
   
 
